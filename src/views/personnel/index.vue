ca0b0112e96ebefcb641956fa311f94f9bea35e4
任婷婷rentt <1542078062@qq.com>
2021-08-31 16:49:26 +0800

    [add]项目列表页面

--- src/views/personnel/index.vue
+++ src/views/personnel/index.vue
@@ -1,10 +1,108 @@
 <template>
-  <div>positionlist</div>
+  <Table
+    class="table"
+    style="margin-top:10px;"
+    :columns="columns"
+    :data-source="data"
+    :pagination="pagination"
+    row-key="id"
+    size="middle"
+    @change="handlePagination">
+    <template #departmentList="{record}">
+      {{ departFormat(record.departmentList) }}
+    </template>
+    <template #gmtCreate="{record}">
+      {{ dateFormat(record.gmtCreate) }}
+    </template>
+  </Table>
 </template>
+
 <script>
-export default {
-  data() {
-    return {};
+import moment from 'moment';
+import { Table} from 'ant-design-vue';
+
+// import { getUsersPage } from '@/api/user';
+
+const columns = [
+  {
+    title: '用户名',
+    key: 'userName',
+    dataIndex: 'userName',
+    align: 'center'
   },
+  {
+    title: '手机号',
+    key: 'telephone',
+    dataIndex: 'telephone',
+    align: 'center'
+  },
+  {
+    title: '所属部门',
+    key: 'departmentList',
+    dataIndex: 'departmentList',
+    slots: { customRender: 'departmentList'},
+    align: 'center'
+  },
+  {
+    title: '创建时间',
+    key: 'gmtCreate',
+    dataIndex: 'gmtCreate',
+    slots: { customRender: 'gmtCreate'},
+    align: 'center'
+  }
+];
+export default {
+  name: 'UserManager',
+  components: {
+    Table
+  },
+  data(){
+    return{
+      columns,
+      data: [],
+      loading: false,
+      pagination: {
+        current: 1,
+        total: 0,
+        pageSize: 10,
+        size: 'small',
+        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total}`
+      }
+    };
+  },
+  created(){
+    // this.getUsersPage();
+  },
+  methods: {
+    handlePagination(pagination){
+      this.getUsersPage(pagination.current);
+    },
+    // async getUsersPage(index,size){
+      // this.loading = true;
+      // index = index || this.pagination.current;
+      // size = size || this.pagination.pageSize;
+      // const {data} = await getUsersPage(index,size);
+      // this.loading = false;
+      // if(data.success){
+      //   const { current,size,total,records} = data.data;
+      //   this.data = records;
+      //   this.pagination = {
+      //     ...this.pagination,
+      //     current,
+      //     total,
+      //     pageSize: size
+      //   };
+      // }
+    // },
+    dateFormat(timer){
+      return moment(timer).format('YYYY/MM/DD hh:mm');
+    },
+    departFormat(departmentList){
+      return departmentList.join(', ');
+    }
+  }
 };
 </script>
+
+<style  scoped>
+</style>
--- src/views/projectList/index.vue
+++ src/views/projectList/index.vue
@@ -1,10 +1,82 @@
 <template>
-  <div>positionlist</div>
+  <a-table :columns="columns" :data-source="data" class="components-table-demo-nested">
+    <template slot="operation">
+      <a-button type="link" size="small">查看</a-button>
+      <a-button type="link" size="small">编辑</a-button>
+      <a-button type="link" size="small">删除</a-button>
+    </template>
+    <a-table
+      slot="expandedRowRender"
+      :columns="innerColumns"
+      :data-source="innerData"
+      :pagination="false"
+    >
+      <span slot="status" > <a-badge status="success" />Finished </span>
+      <template slot="operation">
+        <a-button type="link" size="small">查看</a-button>
+        <a-button type="link" size="small">编辑</a-button>
+        <a-button type="link" size="small">删除</a-button>
+      </template>
+    </a-table>
+  </a-table>
 </template>
 <script>
+const columns = [
+  { title: '项目图片', dataIndex: 'name', key: 'name' },
+  { title: '项目名称', dataIndex: 'platform', key: 'platform' },
+  { title: '项目进程', dataIndex: 'version', key: 'version' },
+  { title: '责任处室', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+  { title: '发起人', dataIndex: 'creator', key: 'creator' },
+  { title: '分管领导', dataIndex: 'creator', key: 'creator' },
+  { title: '日期', dataIndex: 'createdAt', key: 'createdAt' },
+  { title: '项目状态', dataIndex: 'creator', key: 'creator' },
+  { title: '操作', key: 'operation', scopedSlots: { customRender: 'operation' } },
+];
+
+const data = [];
+for (let i = 0; i < 3; ++i) {
+  data.push({
+    key: i,
+    name: 'Screem',
+    platform: 'iOS',
+    version: '10.3.4.5654',
+    upgradeNum: 500,
+    creator: 'Jack',
+    createdAt: '2014-12-24 23:12:00',
+  });
+}
+
+const innerColumns = [
+  { title: 'Date', dataIndex: 'date', key: 'date' },
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Status', key: 'state', scopedSlots: { customRender: 'status' } },
+  { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+  {
+    title: 'Action',
+    dataIndex: 'operation',
+    key: 'operation',
+    scopedSlots: { customRender: 'operation' },
+  },
+];
+
+const innerData = [];
+for (let i = 0; i < 3; ++i) {
+  innerData.push({
+    key: i,
+    date: '2014-12-24 23:12:00',
+    name: 'This is production name',
+    upgradeNum: 'Upgraded: 56',
+  });
+}
+
 export default {
   data() {
-    return {};
+    return {
+      data,
+      columns,
+      innerColumns,
+      innerData,
+    };
   },
 };
 </script>
